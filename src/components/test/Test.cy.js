3bc82ca3edaa37aa6556200a60dc6d2e36f30ff6
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-09-07 13:04:23 +1000

    improve mount

--- src/components/test/Test.cy.js
+++ src/components/test/Test.cy.js
@@ -37,20 +37,8 @@ describe("<Test />", () => {
     });
 
     // Trigger emit event
-    cy.get("button").contains("Click Me").click();
-
-    // Assert event using  Cypress.vue.$on
-    cy.then(() => {
-      Cypress.vue.$on("click", onClickSpy);
+    cy.get("button").contains("Click Me").click().then(() => {
       expect(onClickSpy).to.be.calledOnce;
-    });
-
-    // Assert event using spies
-    cy.get("@onClickSpy").should("have.been.calledOnce");
-
-    // Assert event using listener from vue-test-utils
-    cy.vue().then((wrapper) => {
-      expect(wrapper.emitted("click")).to.have.length(1);
     });
   });
 });
